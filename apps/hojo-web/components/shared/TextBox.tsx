b1f3fd01606aab7bc5cfc9ef21cbe28d401bda2b
nhiennhatt <hiennhatt1804@gmail.com>
2023-02-04 08:09:14 +0700

    add icon text box

--- apps/hojo-web/components/shared/TextBox.tsx
+++
@@ -1,5 +0,0 @@
-import { TextField, TextFieldProps } from "@mui/material";
-
-export default function TextBox (props?: TextFieldProps) {
-    return <TextField { ...props } fullWidth />
-}
--- apps/hojo-web/views/ui-system/text-box.tsx
+++ apps/hojo-web/views/ui-system/text-box.tsx
@@ -1,13 +1,15 @@
 import { Close, Search } from "@mui/icons-material";
-import { Grid, InputAdornment, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
-import { TextBox } from "@web/components/shared";
+import { IconTextBox, TextBox } from "@web/components/shared";
 
 export default function TextBoxUI() {
   return (
     <Grid container spacing={2} padding={2}>
       <Grid xs={12} item>
-        <Typography align="center" fontWeight="bold" variant="h4">Text Box</Typography>
+        <Typography align="center" fontWeight="bold" variant="h4">
+          Text Box
+        </Typography>
       </Grid>
       <Grid md={2} xs={6} item>
         <TextBox label="Default" />
@@ -19,54 +21,19 @@ export default function TextBoxUI() {
         <TextBox error label="Name" defaultValue="Error" />
       </Grid>
       <Grid md={2} xs={6} item>
-        <TextBox
-          placeholder="Search"
-          InputProps={{
-            style: { borderRadius: "40px" },
-            endAdornment: (
-              <InputAdornment position="end">
-                <Search style={{ transform: "scaleX(-1)" }} />
-              </InputAdornment>
-            ),
-          }}
-        />
+        <IconTextBox placeholder="Search" endIcon={<Search />} rounded />
       </Grid>
       <Grid md={2} xs={6} item>
-        <TextBox
-          placeholder="Search"
-          InputProps={{
-            style: { borderRadius: "40px" },
-            endAdornment: (
-              <InputAdornment position="end">
-                <Close />
-              </InputAdornment>
-            ),
-            startAdornment: (
-              <InputAdornment position="start">
-                <Search style={{ transform: "scaleX(-1)" }} />
-              </InputAdornment>
-            ),
-          }}
-        />
+        <IconTextBox placeholder="Search" startIcon={<Search />} endIcon={<Close />} rounded />
       </Grid>
       <Grid md={2} xs={6} item>
-        <TextBox
+        <IconTextBox
+          error
           placeholder="Search"
           defaultValue="Error"
-          error
-          InputProps={{
-            style: { borderRadius: "40px" },
-            endAdornment: (
-              <InputAdornment position="end">
-                <Close />
-              </InputAdornment>
-            ),
-            startAdornment: (
-              <InputAdornment position="start">
-                <Search style={{ transform: "scaleX(-1)" }} />
-              </InputAdornment>
-            ),
-          }}
+          startIcon={<Search />}
+          endIcon={<Close />}
+          rounded
         />
       </Grid>
     </Grid>
